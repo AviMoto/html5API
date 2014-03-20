bd2a50555a0501de50edb72ab443b11f79a907e6
Avi <Avi@admintheweb.com>
2014-03-20 02:06:13 +0200

    create geolocation recognition

--- assets/js/controls.js
+++ assets/js/controls.js
@@ -1,18 +1,36 @@
-window.onload = function(){
-	var audio = document.getElementById("myAudio");
-	var video = document.getElementById("myVideo");
-	var playButton =  document.getElementById("playPause");
-	
-	playButton.onclick = function(){
-		if(!(audio.paused || video.paused)){
-			audio.pause();
-			video.pause();
-			playButton.value = "play";
+function checkGeoLocation() {
+	try {
+		if (navigator.geolocation) {
+			return navigator.geolocation;
 		} else {
-			audio.play();
-			video.play();
-			playButton.value = "pause";
+			return null;
 		}
-	};
+	} catch (e) {
+		return null;
+	}
+}
+window.onload = function() {
+	if(checkGeoLocation()){
+		getLocation();
+	} else {
+		console.log("Missing support for geolocation");
+	}
+	
+	function getLocation(){
+		navigator.geolocation.getCurrentPosition(function(position){
+			var myLatitude = position.coords.latitude;
+			var myLongitude = position.coords.longitude;
+			var mapOptions = {
+					center: new google.maps.LatLng(myLatitude, myLongitude),
+					zoom: 16,
+					mapTypeId: google.maps.MapTypeId.SATELLITE
+			};
+			var map = new google.maps.Map(document.getElementById("myMap"),mapOptions);
+			var marker = new google.maps.Marker({
+			       position: new google.maps.LatLng(myLatitude, myLongitude),
+			       map: map,
+			       title:"this is my location"
+			   });
+		});
+	}
 };
-
